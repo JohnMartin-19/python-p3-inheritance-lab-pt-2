01a27e0c14a4afe60bb39989e0b81ab56349e946
John <johnnkonge2020@gmail.com>
2023-12-01 21:51:46 +0300

    almost there

--- lib/student.py
+++ lib/student.py
@@ -5,7 +5,9 @@ class Student:
     def hello(name):
         print(f'{name},hello there!Im so excited to learn stuff')
 
-    def 
+    def raise_hand():
+        print('pick me bro!')
+    
 
 class ChattyStudent(Student):
     super(self,name).__init__()
